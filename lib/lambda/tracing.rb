9e7f5de6f432122a809389a80244492092c9be41
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-01-07 10:53:21 -0500

    Add wrapped handler and register user handler

--- lib/lambda/tracing.rb
+++ lib/lambda/tracing.rb
@@ -7,10 +7,47 @@ module Lambda
   module Tracing
     class Error < StandardError; end
 
-    def self.handler
+    def self.wrapped_handler(event:, context:)
       init_tracer
 
       puts OpenTracing.global_tracer
+      puts context.inspect
+
+      tags = {
+        'component' => 'ruby-lambda-wrapper',
+        'lambda_arn' => context.invoked_function_arn,
+        'aws_function_name' => context.function_name,
+        'aws_function_version' => context.function_version,
+        'aws_execution_env' => ENV['AWS_EXECUTION_ENV'],
+        'function_wrapper_version' => "lambda-tracing-#{Lambda::Tracing::VERSION}",
+      }
+
+      tags = tags.merge(tags_from_arn(context.invoked_function_arn))
+
+      response = nil
+      @tracer.start_active_trace("lambda_ruby_#{context.function_name}", tags: tags) do |scope|
+        response = @handler.call(event: event, context: context)
+      end
+
+      @reporter.flush
+      response
+    end
+
+    def self.tags_from_arn(arn)
+      _, _, _, region, account_id, resource_type, resource, qualifier
+
+      tags = {
+        'aws_region' => region,
+        'aws_account_id' => account_id,
+      }
+      tags['aws_function_qualifier'] = qualifier if qualifier && resource_type == 'function'
+      tags['event_function_qualifier'] = qualifier if qualifier && resource_type == 'event-source-mapping'
+
+      tags
+    end
+
+    def self.register_handler(&handler)
+      @handler = handler
     end
 
     def self.init_tracer
@@ -23,7 +60,7 @@ module Lambda
       headers['X-SF-Token'] = access_token if !access_token.empty
       encoder = Jaeger::Client::Encoders::ThriftEncoder.new(service_name: service_name)
       sender = Jaeger::Client::HttpSender.new(url: ingest_url, headers: headers, encoder: encoder, logger: Logger.new(STDOUT))
-      reporter = Jaeger::Client::Reporters::RemoteReporter.new(sender: sender, flush_interval: 1)
+      @reporter = Jaeger::Client::Reporters::RemoteReporter.new(sender: sender, flush_interval: 1)
 
       # propagation format configuration
       injectors = {
@@ -34,7 +71,7 @@ module Lambda
       }
 
       OpenTracing.global_tracer = Jaeger::Client.build(
-        reporter: reporter,
+        reporter: @reporter,
         injectors: injectors,
         extractors: extractors
       )
